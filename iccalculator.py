1e583df0ea086a2ad164b170b8940053e6a5793c
guano <49875739+watterle@users.noreply.github.com>
2024-06-10 18:40:44 +0200

    Update iccalculator.py

--- iccalculator.py
+++ iccalculator.py
@@ -5,34 +5,6 @@ import os
 import scipy.integrate as sci
 import sys
 
-tags=["\"ENG\"", "\"USA\"", "\"FRA\"", "\"RAJ\"", "\"MAN\"", "\"EFR\"", "\"CAN\"", "\"AST\"", "\"CHI\"", "\"SAF\"","\"PER\"", "\"NZL\"", "\"SOV\"", "\"GER\"", "\"ITA\"", "\"HUN\"", "\"ROM\"", "\"BUL\"", "\"VIC\"", "\"JAP\"", "\"MAN\"", "\"FIN\"", "\"SLO\"", "\"SPR\"", "\"LAT\"", "\"YUG\"", "\"GRE\"", "\"ALB\"", "\"NOR\"", "\"POR\"", "\"IRE\"", "\"ETH\"", "\"IRQ\"", "\"SIA\"", "\"VEN\"", "\"MON\"", "\"TAN\"", "\"PAR\"", "\"PRC\"", "\"BEL\"", "\"INS\"", "\"AUS\"", "\"POL\"", "\"CZE\"", "\"HOL\""]
-majors=["\"GER\"","\"SOV\"","\"ENG\"","\"JAP\"","\"USA\"","\"ITA\"","\"FRA\""]
-alliedminors=["\"RAJ\"", "\"CAN\"", "\"AST\"", "\"SAF\"", "\"NZL\""]
-axisminors=["\"HUN\"", "\"ROM\"", "\"BUL\"","\"SPR\"","\"SLO\"","\"MAN\""]
-minors=alliedminors+axisminors
-
-
-#Sets which countries to analyze
-inputtag=majors
-#Sets "save" folder path
-absolute_path = os.path.dirname(__file__)
-relative_path = "save\\"
-relative_path_images = "images\\"
-folderpath = os.path.join(absolute_path, relative_path)
-folderpathimages = os.path.join(absolute_path, relative_path_images)
-if not os.path.exists(folderpathimages):
-    os.makedirs(folderpathimages)
-nautosaves=len([entry for entry in os.listdir(folderpath) if os.path.isfile(os.path.join(folderpath, entry))])
-
-
-tagic=[ [0]*len(inputtag) for i in range(nautosaves+1)] #monthly IC in columns
-tagic[0]=inputtag  #tags
-navtagic=[ [0]*len(inputtag) for i in range(nautosaves+1)] #monthly IC in columns
-navtagic[0]=inputtag  #tags
-contractic=[ [0]*len(inputtag) for i in range(nautosaves+1)] #monthly IC in columns
-contractic[0]=inputtag  #tags
-
-
 def listidx(L, obj):
     if obj in L:
         return L.index(obj)
@@ -120,127 +92,166 @@ def monthlyic(fullpath,row): #ic calculator
             else:
                 navtagic[row][count]=0
 
-def contractsic(fullpath,row): #contracts counter
-    with open(fullpath, 'r', encoding="utf8") as fp:
-        flag_found=0
-        content = fp.read()
-        custom_regex=r'_count_variable='
-        indices_object = re.finditer(custom_regex, string=content) #looks for contracts counters
-        indices = [index.start() for index in indices_object]
-        contractcountryic=[]
-        ic=0
-        for jj in tags:
-            flag_found=0
-            for x in indices:
-                country="\""+content[x-3:x].upper()+"\""
-                if jj == country:
-                    if country=="\"CZE\"": 
-                        country="\"SLO\""
-                    contractcountryic.append(country)
-                    idx2=content.find('\n', x, x+30) #registers the line's daily IC
-                    ic=float(content[x+16:idx2])
-                    contractcountryic.append(ic)
-                    flag_found=1
-            if flag_found==0:
-                contractcountryic.append(jj)
-                contractcountryic.append(0)
-        #print(contractcountryic)
-        for count,value in enumerate(contractic[0]): #build output table with ICs
-            position=rindex(contractcountryic,value)
-            if (position!=-1) and ((position+1)<len(contractcountryic)):
-                contractic[row][count]=round(contractcountryic[position+1],1)
-            else:
-                contractic[row][count]=0
+tags=["\"ENG\"", "\"USA\"", "\"FRA\"", "\"RAJ\"", "\"MAN\"", "\"EFR\"", "\"CAN\"", "\"AST\"", "\"CHI\"", "\"SAF\"","\"PER\"", "\"NZL\"", "\"SOV\"", "\"GER\"", "\"ITA\"", "\"HUN\"", "\"ROM\"", "\"BUL\"", "\"VIC\"", "\"JAP\"", "\"MAN\"", "\"FIN\"", "\"SLO\"", "\"SPR\"", "\"LAT\"", "\"YUG\"", "\"GRE\"", "\"ALB\"", "\"NOR\"", "\"POR\"", "\"IRE\"", "\"ETH\"", "\"IRQ\"", "\"SIA\"", "\"VEN\"", "\"MON\"", "\"TAN\"", "\"PAR\"", "\"PRC\"", "\"BEL\"", "\"INS\"", "\"AUS\"", "\"POL\"", "\"CZE\"", "\"HOL\""]
+majors=["\"GER\"","\"SOV\"","\"ENG\"","\"JAP\"","\"USA\"","\"ITA\"","\"FRA\""]
+alliedminors=["\"RAJ\"", "\"CAN\"", "\"AST\"", "\"SAF\"", "\"NZL\"", "\"POL\""]
+axisminors=["\"HUN\"", "\"ROM\"", "\"BUL\"","\"SPR\"","\"SLO\"","\"MAN\""]
+minors=alliedminors+axisminors
+all=minors+majors
+
+loop = {"majors" : majors,
+        "allies" : alliedminors,
+        "axis" : axisminors,
+        "faction" : all
+        }
+for category, taglist in loop.items():
+    inputtag = taglist
+    #Sets which countries to analyze
+    #Sets "save" folder path
+    absolute_path = os.path.dirname(__file__)
+    relative_path = "save\\"
+    relative_path_images = category+"\\"
+    folderpath = os.path.join(absolute_path, relative_path)
+    folderpathimages = os.path.join(absolute_path, relative_path_images)
+    if not os.path.exists(folderpathimages):
+        os.makedirs(folderpathimages)
+    nautosaves=len([entry for entry in os.listdir(folderpath) if os.path.isfile(os.path.join(folderpath, entry))])
 
 
-#calculates monthlyic for every file in /save. Edit this if you haven't saved your files as save1, save2,...
-for i in range(nautosaves):  
-    filename=folderpath+"save"+str(i+1)+".hoi4"
-    print_progress_bar(i+1, nautosaves, "Wait...")
-    monthlyic(filename,i+1)
-    contractsic(filename,i+1)
-
-
-
-#apply moving average for instant ic and integrates for cumulative ic
-dat = np.array(tagic[1:])*30
-contractdat = np.diff(np.array(contractic[1:]), axis=0, prepend=0)*2000 
-dat=dat+contractdat
-for i in range(len(inputtag)):
-    dat[1:nautosaves-1,i]=moving_avg(dat[:,i],3)
-result = sci.cumtrapz(dat, axis=0, dx=1)
-dat = np.around(dat, decimals=3)
-result = np.around(result, decimals=3)
-
-navdat = np.array(navtagic[1:])*30
-for i in range(len(inputtag)):
-    navdat[1:nautosaves-1,i]=moving_avg(navdat[:,i],3)
-navresult = sci.cumtrapz(navdat, axis=0, dx=1)
-navdat = np.around(navdat, decimals=3)
-navresult = np.around(navresult, decimals=3)
+    tagic=[ [0]*len(inputtag) for i in range(nautosaves+1)] #monthly IC in columns
+    tagic[0]=inputtag  #tags
+    navtagic=[ [0]*len(inputtag) for i in range(nautosaves+1)] #monthly IC in columns
+    navtagic[0]=inputtag  #tags
+    contractic=[ [0]*len(inputtag) for i in range(nautosaves+1)] #monthly IC in columns
+    contractic[0]=inputtag  #tags
+    #calculates monthlyic for every file in /save. Edit this if you haven't saved your files as save1, save2,...
+    for i in range(nautosaves):  
+        filename=folderpath+"autosave_"+str(nautosaves-i)+".hoi4"
+        print_progress_bar(i+1, nautosaves, "Wait...")
+        monthlyic(filename,i+1)
 
 
 
-#saves results on txt files
-#with open('matrix.txt', 'w') as testfile:
-#    for row in dat:
-#        testfile.write(','.join([str(a) for a in row]) + '\n')
-#with open('matrixcum.txt', 'w') as testfile:
-#    for row in result:
-#        testfile.write(','.join([str(a) for a in row]) + '\n')
+    #apply moving average for instant ic and integrates for cumulative ic
+    dat = np.array(tagic[1:])*30
+    for i in range(len(inputtag)):
+        dat[1:nautosaves-1,i]=moving_avg(dat[:,i],3)
+    result = sci.cumtrapz(dat, axis=0, dx=1)
+    dat = np.around(dat, decimals=3)
+    result = np.around(result, decimals=3)
+
+    navdat = np.array(navtagic[1:])*30
+    for i in range(len(inputtag)):
+        navdat[1:nautosaves-1,i]=moving_avg(navdat[:,i],3)
+    navresult = sci.cumtrapz(navdat, axis=0, dx=1)
+    navdat = np.around(navdat, decimals=3)
+    navresult = np.around(navresult, decimals=3)
 
 
-x=range(1,nautosaves)
-x_ticks = [0, 12, 24, 36, 48, 60, 72]
-x_labels = ['1936', '1937', '1938', '1939', '1940', '1941', '1942']
-#plots results
-plt.rcParams.update({'font.size': 15})
-plt.figure("Produced Land IC")
-plt.title("Produced Land IC") 
-plt.xlabel("Years") 
-plt.ylabel("Produced Land IC") 
-plt.plot(x,result) 
-plt.xticks(x_ticks, x_labels)
-plt.legend(tagic[0])
-plt.grid( linestyle = '--', linewidth = 0.5)
-filename=folderpathimages+"ProducedLandIC.png"
-plt.savefig(filename, dpi=600, bbox_inches='tight' )
 
-x=range(0,nautosaves)
-plt.figure("Monthly Land IC")
-plt.title("Monthly Land IC") 
-plt.xlabel("Year") 
-plt.ylabel("Monthly Land IC") 
-plt.plot(x,dat) 
-plt.grid( linestyle = '--', linewidth = 0.5)
-plt.xticks(x_ticks, x_labels)
-plt.legend(tagic[0])
-filename=folderpathimages+"MonthlyLandIC.png"
-plt.savefig(filename, dpi=600, bbox_inches='tight' )
+    x=range(1,nautosaves)
+    x_ticks = [0, 12, 24, 36, 48, 60, 72, 84, 96]
+    x_labels = ['1936', '1937', '1938', '1939', '1940', '1941', '1942', '1943', '1944']
+    if inputtag != all:
+        #plots results
+        plt.rcParams.update({'font.size': 15})
+        plt.figure("Produced MIL IC")
+        plt.title("Produced MIL IC") 
+        plt.xlabel("Years") 
+        plt.ylabel("Produced MIL IC") 
+        plt.plot(x,result) 
+        plt.xticks(x_ticks, x_labels)
+        plt.legend(tagic[0])
+        plt.grid( linestyle = '--', linewidth = 0.5)
+        filename=folderpathimages+"ProducedLandIC.png"
+        plt.savefig(filename, dpi=600, bbox_inches='tight' )
+        plt.clf()
+        x=range(0,nautosaves)
+        plt.figure("Monthly MIL IC")
+        plt.title("Monthly MIL IC") 
+        plt.xlabel("Year") 
+        plt.ylabel("Monthly MIL IC") 
+        plt.plot(x,dat) 
+        plt.grid( linestyle = '--', linewidth = 0.5)
+        plt.xticks(x_ticks, x_labels)
+        plt.legend(tagic[0])
+        filename=folderpathimages+"MonthlyLandIC.png"
+        plt.savefig(filename, dpi=600, bbox_inches='tight' )
+        plt.clf()
+        x=range(1,nautosaves)
+    if inputtag == majors:
+        #plots results
+        plt.figure("Cumulative NAV IC")
+        plt.title("Cumulative NAV IC") 
+        plt.xlabel("Years") 
+        plt.ylabel("Cumulative NAV IC") 
+        plt.plot(x,navresult) 
+        plt.xticks(x_ticks, x_labels)
+        plt.legend(navtagic[0])
+        plt.grid( linestyle = '--', linewidth = 0.5)
+        filename=folderpathimages+"CumulativeNavalIC.png"
+        plt.savefig(filename, dpi=600, bbox_inches='tight' )
+        plt.clf()
+        x=range(0,nautosaves)
+        plt.figure("Monthly Naval IC")
+        plt.title("Monthly Naval IC") 
+        plt.xlabel("Year") 
+        plt.ylabel("Monthly Naval IC") 
+        plt.plot(x,navdat) 
+        plt.grid( linestyle = '--', linewidth = 0.5)
+        plt.xticks(x_ticks, x_labels)
+        plt.legend(navtagic[0])
+        filename=folderpathimages+"MonthlyNavalIC.png"
+        plt.savefig(filename, dpi=600, bbox_inches='tight' )
+        plt.clf()
+    if inputtag == all:
+        factions=["ALLIES", "AXIS", "COMINTERN","GEAPCS"]
+        factionmap =	{
+          "\"GER\"":1,"\"SOV\"":2,"\"ENG\"":0,"\"JAP\"":3,"\"USA\"":0,"\"ITA\"":1,"\"FRA\"":0,
+          "\"RAJ\"":0, "\"CAN\"":0, "\"AST\"":0, "\"SAF\"":0, "\"NZL\"":0, "\"POL\"":0,
+          "\"HUN\"":1, "\"ROM\"":1, "\"BUL\"":1,"\"SPR\"":1,"\"SLO\"":1,"\"MAN\"":3
+        }
+        factionic=[ [0]*len(factions) for i in range(nautosaves+1)] #monthly IC in columns
+        factionic[0]=factions  #tags
 
-x=range(1,nautosaves)
-#plots results
-plt.figure("Cumulative Naval IC")
-plt.title("Cumulative Naval IC") 
-plt.xlabel("Years") 
-plt.ylabel("Cumulative Naval IC") 
-plt.plot(x,navresult) 
-plt.xticks(x_ticks, x_labels)
-plt.legend(navtagic[0])
-plt.grid( linestyle = '--', linewidth = 0.5)
-filename=folderpathimages+"CumulativeNavalIC.png"
-plt.savefig(filename, dpi=600, bbox_inches='tight' )
+        for rowindex, rows in enumerate(tagic): 
+            for columnindex, column in enumerate(rows): 
+                if rowindex>0:
+                    factionic[rowindex][factionmap[tagic[0][columnindex]]] += tagic[rowindex][columnindex]
 
-x=range(0,nautosaves)
-plt.figure("Monthly Naval IC")
-plt.title("Monthly Naval IC") 
-plt.xlabel("Year") 
-plt.ylabel("Monthly Naval IC") 
-plt.plot(x,navdat) 
-plt.grid( linestyle = '--', linewidth = 0.5)
-plt.xticks(x_ticks, x_labels)
-plt.legend(navtagic[0])
-filename=folderpathimages+"MonthlyNavalIC.png"
-plt.savefig(filename, dpi=600, bbox_inches='tight' )
+        factiondat = np.array(factionic[1:])*30
+        for i in range(len(factions)):
+            factiondat[1:nautosaves-1,i]=moving_avg(factiondat[:,i],3)
+        factionresult = sci.cumtrapz(factiondat, axis=0, dx=1)
+        factiondat = np.around(factiondat, decimals=3)
+        factionresult = np.around(factionresult, decimals=3)
 
+        x=range(1,nautosaves)
+        x_ticks = [0, 12, 24, 36, 48, 60, 72, 84, 96]
+        x_labels = ['1936', '1937', '1938', '1939', '1940', '1941', '1942', '1943', '1944']
 
+        #plots results
+        plt.rcParams.update({'font.size': 15})
+        plt.figure("Produced MIL IC")
+        plt.title("Produced MIL IC") 
+        plt.xlabel("Years") 
+        plt.ylabel("Produced MIL IC") 
+        plt.plot(x,factionresult) 
+        plt.xticks(x_ticks, x_labels)
+        plt.legend(factionic[0])
+        plt.grid( linestyle = '--', linewidth = 0.5)
+        filename=folderpathimages+"ProducedLandIC.png"
+        plt.savefig(filename, dpi=600, bbox_inches='tight' )
+        plt.clf()
+        x=range(0,nautosaves)
+        plt.figure("Monthly MIL IC")
+        plt.title("Monthly MIL IC") 
+        plt.xlabel("Year") 
+        plt.ylabel("Monthly MIL IC") 
+        plt.plot(x,factiondat) 
+        plt.grid( linestyle = '--', linewidth = 0.5)
+        plt.xticks(x_ticks, x_labels)
+        plt.legend(factionic[0])
+        filename=folderpathimages+"MonthlyLandIC.png"
+        plt.savefig(filename, dpi=600, bbox_inches='tight' )
+        plt.clf()
